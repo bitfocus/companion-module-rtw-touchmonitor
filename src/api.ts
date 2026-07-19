ce0ac231d141eb929ff50fce7dc4f63f3b663bbe
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2026-07-19 20:47:48 +1000

    add new api commands supported in fw v2.2

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,8 @@ export const OscPaths = {
 		Start: (appId: number | 'all'): string => `/metering/${appId}/start`,
 		Stop: (appId: number | 'all'): string => `/metering/${appId}/stop`,
 		Reset: (appId: number | 'all'): string => `/metering/${appId}/reset`,
+		InRouting: (appId: number | 'all'): string => `/metering/${appId}/in_routing`,
+		OutRouting: (appId: number | 'all'): string => `/metering/${appId}/out_routing`,
 	},
 	Monitoring: {
 		SetVolume: (): string => '/monitoring/volume',
@@ -13,12 +15,51 @@ export const OscPaths = {
 		EnableHeadphones: (): string => '/monitoring/phones',
 		SelectInput: (): string => '/monitoring/input',
 		SelectOutput: (): string => '/monitoring/output',
+		Downmix: (): string => '/monitoring/downmix',
+		SurroundAttenuation: (): string => '/monitoring/surr_att',
+		LfBoost: (): string => '/monitoring/lf_boost',
+		Mono: (): string => '/monitoring/mono',
+		InRouting: (): string => '/monitoring/in_routing',
+		OutRouting: (): string => '/monitoring/out_routing',
+		Input: {
+			Trim: (inputIdx: number): string => `/monitoring/in/${inputIdx}/trim`,
+			Mute: (inputIdx: number): string => `/monitoring/in/${inputIdx}/mute`,
+			Delay: (inputIdx: number): string => `/monitoring/in/${inputIdx}/delay`,
+		},
+		Output: {
+			Level: (outputIdx: number, speakerIdx: number): string => `/monitoring/out/${outputIdx}/${speakerIdx}/level`,
+			Polarity: (outputIdx: number, speakerIdx: number): string =>
+				`/monitoring/out/${outputIdx}/${speakerIdx}/polarity`,
+			Type: (outputIdx: number, speakerIdx: number): string => `/monitoring/out/${outputIdx}/${speakerIdx}/type`,
+			BassManagementWeighting: (outputIdx: number, speakerIdx: number): string =>
+				`/monitoring/out/${outputIdx}/${speakerIdx}/bm_weighting`,
+			Delay: (outputIdx: number, speakerIdx: number): string => `/monitoring/out/${outputIdx}/${speakerIdx}/delay`,
+			Eq: {
+				Enable: (outputIdx: number, speakerIdx: number): string =>
+					`/monitoring/out/${outputIdx}/${speakerIdx}/eq/enable`,
+				BandActive: (outputIdx: number, speakerIdx: number, bandIdx: number): string =>
+					`/monitoring/out/${outputIdx}/${speakerIdx}/eq/${bandIdx}/active`,
+				BandType: (outputIdx: number, speakerIdx: number, bandIdx: number): string =>
+					`/monitoring/out/${outputIdx}/${speakerIdx}/eq/${bandIdx}/type`,
+				BandFrequency: (outputIdx: number, speakerIdx: number, bandIdx: number): string =>
+					`/monitoring/out/${outputIdx}/${speakerIdx}/eq/${bandIdx}/frequency`,
+				BandGain: (outputIdx: number, speakerIdx: number, bandIdx: number): string =>
+					`/monitoring/out/${outputIdx}/${speakerIdx}/eq/${bandIdx}/gain`,
+				BandQ: (outputIdx: number, speakerIdx: number, bandIdx: number): string =>
+					`/monitoring/out/${outputIdx}/${speakerIdx}/eq/${bandIdx}/q`,
+			},
+		},
 	},
 	Talkback: {
 		SetMicGain: (appId: number | 'all'): string => `/talkback/${appId}/gain`,
 		Enable: (appId: number | 'all'): string => `/talkback/${appId}/active`,
+		SetVolume: (appId: number | 'all'): string => `/talkback/${appId}/volume`,
+		InRouting: (appId: number | 'all'): string => `/talkback/${appId}/in_routing`,
+		OutRouting: (appId: number | 'all'): string => `/talkback/${appId}/out_routing`,
 	},
 	Device: {
 		PhantomPower: (): string => '/device/phantom',
+		PhonesGain: (): string => '/device/phones_gain',
+		XlrGain: (): string => '/device/xlr_gain',
 	},
 }
